9d6676c93a11c15ad929c3f5ceda7abe9458e595
MichalZelazko <242149@edu.p.lodz.pl>
2023-10-20 15:19:38 +0200

    categories legend done

--- calendar-frontend/src/App.tsx
+++ calendar-frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import useFetchCategories from "./hooks/useFetchCategories";
 import Header from "./components/header/Header";
-// import {
-//   CategoryTile,
-//   CategoryTileAttributes,
-// } from "./components/categoryTile/CategoryTile";
+import {
+  CategoryTile,
+  CategoryTileProps,
+} from "./components/categoryTile/CategoryTile";
 import "./App.css";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
@@ -17,33 +17,49 @@ function App() {
 
   return (
     <>
-      <Header title="Calendar" />
-      <div className="container mx-auto">
-        {loading && <div className="loading">Loading...</div>}
-        {error && <div className="error">Error...</div>}
-        {data && (
-          <>
-            <FullCalendar
-              initialView="dayGridMonth"
-              plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-              headerToolbar={{
-                left: "today",
-                center: "prev title next",
-                right: "dayGridMonth,timeGridWeek,timeGridDay",
-              }}
-              allDaySlot={false}
-              slotLabelFormat={{
-                hour12: false,
-                hour: "numeric",
-                minute: "2-digit",
-                omitZeroMinute: false,
-                meridiem: false,
-              }}
-              fixedWeekCount={false}
-              firstDay={1}
-            />
-          </>
-        )}
+      <div className="h-screen flex flex-row font-mono">
+        <div className="basis-1/6 bg-gray-100 border-r border-b border-gray-200">
+          <Header title="Calendar" />
+          <div className="w-full p-3">
+            <h2 className="text-xl font-bold">Categories</h2>
+            <ul className="mt-2">
+              {data &&
+                data.map((category: CategoryTileProps) => (
+                  <li key={category.id}>
+                    <CategoryTile {...category} />
+                  </li>
+                ))}
+            </ul>
+          </div>
+        </div>
+        <div className="basis-5/6 font-mono py-5 px-10 h-full">
+          {loading && <div className="loading">Loading...</div>}
+          {error && <div className="error">Error...</div>}
+          {data && (
+            <>
+              <FullCalendar
+                initialView="dayGridMonth"
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                headerToolbar={{
+                  left: "today",
+                  center: "prev title next",
+                  right: "dayGridMonth,timeGridWeek,timeGridDay",
+                }}
+                allDaySlot={false}
+                slotLabelFormat={{
+                  hour12: false,
+                  hour: "numeric",
+                  minute: "2-digit",
+                  omitZeroMinute: false,
+                  meridiem: false,
+                }}
+                locale={"en-gb"}
+                fixedWeekCount={false}
+                firstDay={1}
+              />
+            </>
+          )}
+        </div>
       </div>
     </>
   );
--- calendar-frontend/src/components/header/Header.tsx
+++ calendar-frontend/src/components/header/Header.tsx
@@ -5,8 +5,8 @@ type HeaderProps = {
 export const Header = (props: HeaderProps) => {
   return (
     <>
-      <header className="py-5 bg-gray-200 flex flex-row align-middle justify-center">
-        <h1 className="text-2xl">{props.title}</h1>
+      <header className="py-5 bg-white flex flex-row align-middle justify-center border-b border-b-gray-200 font-mono">
+        <h1 className="text-[1.75rem]">{props.title}</h1>
       </header>
     </>
   );
